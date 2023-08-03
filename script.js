0c5e8de37de113e01f5dbd636338d09ec4343063
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-03 08:34:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const items = document.querySelectorAll('.item');
+    const dropZones = document.querySelectorAll('.drop-zone');
+
+    items.forEach(item => {
+        item.addEventListener('dragstart', handleDragStart);
+        item.addEventListener('dblclick', handleDoubleClick);
+    });
+
+    dropZones.forEach(zone => {
+        zone.addEventListener('dragover', handleDragOver);
+        zone.addEventListener('drop', handleDrop);
+    });
+
+    function handleDragStart(event) {
+        event.dataTransfer.setData('text/plain', event.target.id);
+    }
+
+    function handleDragOver(event) {
+        event.preventDefault();
+    }
+
+    function handleDrop(event) {
+        event.preventDefault();
+        const itemId = event.dataTransfer.getData('text/plain');
+        const item = document.getElementById(itemId);
+
+        if (!item) return;
+
+        if (event.target.classList.contains('drop-zone')) {
+            event.target.appendChild(item);
+        }
+    }
+
+    function handleDoubleClick(event) {
+        const itemId = event.target.id;
+        const unrankedDropZone = document.getElementById('unranked-drop-zone');
+        const item = document.getElementById(itemId);
+
+        if (unrankedDropZone.contains(item)) return;
+
+        unrankedDropZone.appendChild(item);
+    }
+});
